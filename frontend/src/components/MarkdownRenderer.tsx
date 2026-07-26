85a3d367bb4d29777a3082f4667a7c62b625dc80
daddylonglegsai-collab <daddy.longlegs.ai@gmail.com>
2026-07-26 12:20:52 +0330

    Create MarkdownRenderer.tsx

---
+++ frontend/src/components/MarkdownRenderer.tsx
@@ -0,0 +1,14 @@
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import rehypeHighlight from "rehype-highlight";
+
+export default function MarkdownRenderer({content}) {
+  return (
+    <ReactMarkdown
+      remarkPlugins={[remarkGfm]}
+      rehypePlugins={[rehypeHighlight]}
+    >
+      {content}
+    </ReactMarkdown>
+  );
+}
